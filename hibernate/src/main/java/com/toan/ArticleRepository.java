bc064fee38baa0510dc6e656a6372f205e7b6ade
CHU Minh Toan <toan.portable@gmail.com>
2026-06-24 22:18:29 +0200

    add hibernate tests

--- hibernate/src/main/java/com/toan/ArticleRepository.java
+++ hibernate/src/main/java/com/toan/ArticleRepository.java
@@ -2,8 +2,11 @@ package com.toan;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import org.hibernate.StatelessSession;
+
+import java.util.List;
 
 @ApplicationScoped
 public class ArticleRepository {
@@ -11,8 +14,32 @@ public class ArticleRepository {
   @Inject
   StatelessSession statelessSession;
 
+  @Inject
+  EntityManager entityManager;
+
   @Transactional
   public void upsertArticle(Article article) {
     statelessSession.upsert(article);
+  }
+
+  @Transactional
+  public void saveArticle(Article article) {
+    statelessSession.createNativeQuery("""
+        INSERT INTO Article (id, name) VALUES (:id, :name)
+                ON CONFLICT (id) DO UPDATE SET name = EXCLUDED.name
+        """)
+      .setParameter("id", article.getId())
+      .setParameter("name", article.getName())
+      .executeUpdate();
+  }
+
+  @Transactional
+  public void mergeArticle(Article article) {
+    entityManager.merge(article);
+  }
+
+  @Transactional
+  public void upsertMultiple(List<Article> articles) {
+    statelessSession.upsertMultiple(articles);
   }
 }
--- hibernate/src/test/java/com/toan/ArticleTest.java
+++ hibernate/src/test/java/com/toan/ArticleTest.java
@@ -3,7 +3,6 @@ package com.toan;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.virtual.threads.VirtualThreads;
 import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -24,25 +23,36 @@ class ArticleTest {
   ExecutorService executorService;
 
   @Test
-  @Transactional
   void shouldSaveArticleConcurrently() {
     assertThatNoException().isThrownBy(this::verifySave);
   }
 
   @Test
-  @Transactional
   void shouldSaveArticleConcurrentlyBis() {
     assertThatNoException().isThrownBy(this::verifySaveBis);
+  }
+
+  @Test
+  void shouldSaveArticleConcurrentlyByQuery() {
+    assertThatNoException().isThrownBy(this::verifySaveArticle);
+  }
+
+  @Test
+  void shouldMergeArticleConcurrently() {
+    assertThatNoException().isThrownBy(this::verifyMerge);
+  }
+
+  @Test
+  void shouldUpsertMultipleConcurrently() {
+    assertThatNoException().isThrownBy(this::verifyUpsertMultiple);
   }
 
   private void verifySave() {
     Article article = new Article(1L, "article");
     List<Future<?>> list = new ArrayList<>();
     try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
-      for (int i = 0; i < 10; i++) {
-        var future = executor.submit(() -> {
-          articleRepository.upsertArticle(article);
-        });
+      for (int i = 0; i < 5; i++) {
+        var future = executor.submit(upsertArticle(article));
         list.add(future);
       }
       Futures.joinOrThrow(list);
@@ -52,12 +62,67 @@ class ArticleTest {
   private void verifySaveBis() {
     Article article = new Article(1L, "article");
     List<Future<?>> list = new ArrayList<>();
-      for (int i = 0; i < 10; i++) {
-        var future = executorService.submit(() -> {
-          articleRepository.upsertArticle(article);
-        });
+      for (int i = 0; i < 5; i++) {
+        var future = executorService.submit(upsertArticle(article));
         list.add(future);
       }
       Futures.joinOrThrow(list);
+  }
+
+  private void verifySaveArticle() {
+    Article article = new Article(1L, "article");
+    List<Future<?>> list = new ArrayList<>();
+    try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+      for (int i = 0; i < 5; i++) {
+        var future = executor.submit(saveArticle(article));
+        list.add(future);
+      }
+      Futures.joinOrThrow(list);
+    }
+  }
+
+  private void verifySaveArticleBis() {
+    Article article = new Article(1L, "article");
+    List<Future<?>> list = new ArrayList<>();
+    for (int i = 0; i < 100; i++) {
+      var future = executorService.submit(saveArticle(article));
+      list.add(future);
+    }
+    Futures.joinOrThrow(list);
+  }
+  private void verifyMerge() {
+    Article article = new Article(1L, "article");
+    List<Future<?>> list = new ArrayList<>();
+    for (int i = 0; i < 5; i++) {
+      var future = executorService.submit(mergeArticle(article));
+      list.add(future);
+    }
+    Futures.joinOrThrow(list);
+  }
+
+  private void verifyUpsertMultiple() {
+    Article article = new Article(1L, "article");
+    List<Future<?>> list = new ArrayList<>();
+    for (int i = 0; i < 5; i++) {
+      var future = executorService.submit(upsertMultiple(article));
+      list.add(future);
+    }
+    Futures.joinOrThrow(list);
+  }
+
+  private Runnable upsertArticle(Article article) {
+    return () -> articleRepository.upsertArticle(article);
+  }
+
+  private Runnable saveArticle(Article article) {
+    return () -> articleRepository.saveArticle(article);
+  }
+
+  private Runnable upsertMultiple(Article article) {
+    return () -> articleRepository.upsertMultiple(List.of(article));
+  }
+
+  private Runnable mergeArticle(Article article) {
+    return () -> articleRepository.mergeArticle(article);
   }
 }
